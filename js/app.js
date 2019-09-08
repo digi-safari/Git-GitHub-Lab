0e3b73fbcfddf48af3e47a92ec842cbd106ba46d
dongli <dongli.pzh@gmail.com>
2019-09-08 19:41:01 -0400

    add leap year check section

--- js/app.js
+++ js/app.js
@@ -12,3 +12,28 @@ function nameCheck(){
         document.getElementById("welcome_msg").innerHTML = "Welcome!";     
     }
 }
+
+function checkLeapYear() {
+    //clear for resubmits
+    document.getElementById("result").innerHTML = "";
+
+    var year = document.getElementById("year").value;
+    var answer = "Not a leap year"
+
+    if (year % 4 == 0) {
+        answer = "It's a leap year!";
+    }
+    if (year % 100 == 0) {
+        answer = "Not a leap year";
+    }
+    if (year % 100 == 0 && year % 400 == 0) {
+        answer = "It's a leap year!";
+    }
+
+
+
+    document.getElementById("result").innerHTML = answer;
+
+    // If the year can be evenly divided by 100, it is NOT a leap year, unless;
+    // The year is also evenly divisible by 400. Then it is a leap year.
+}
